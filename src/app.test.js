357f5e288d9ba21f34f02d44f4644eb6d3924907
ramicoder <ramidaood@anonymous>
2026-09-06 19:12:34 +0300

    coded the receive attack function along with some tests

--- src/app.test.js
+++ src/app.test.js
@@ -1,18 +1,19 @@
 import { createShip, createGameboard } from "./factories.js";
 
 
-let myShip = createShip(5);
-let board = createGameboard();
+let ship1 = createShip(1);
+let ship5 = createShip(5);
 
-test("test positioning", function() {
-    expect(board.placeShip(myShip, 1, 1, "vertical")).toEqual(
+test("Test positioning", function() {
+    let board = createGameboard();
+    expect(board.placeShip(ship5, 1, 1, "vertical")).toEqual(
         [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 5, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 5, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 5, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 5, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 5, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,9 +23,31 @@ test("test positioning", function() {
 })
 
 test("Invalid positioning", function() {
-    expect( () => (board.placeShip(myShip, -1, 1, "vertical"))).toThrow("Position is not valid");
+    let board = createGameboard();
+    expect( () => (board.placeShip(ship1, -1, 1, "vertical"))).toThrow("Position is not valid");
 })
 
 test("Invalid orientation", function() {
-    expect( () => (board.placeShip(myShip, 1, 1, "verticaal"))).toThrow("Orientation is not valid");
+    let board = createGameboard();
+    expect( () => (board.placeShip(ship1, 1, 1, "verticaal"))).toThrow("Orientation is not valid");
+})
+
+test("Hitting a ship until sunken", function() {
+    let board = createGameboard();
+    //placing the ship
+    board.placeShip(ship5, 1, 1, "vertical")
+    //attacking the ship
+    board.receiveAttack(1, 1);
+    board.receiveAttack(2, 1);
+    board.receiveAttack(3, 1);
+    board.receiveAttack(4, 1);
+    board.receiveAttack(5, 1);
+    expect( ship5.isSunk()).toBe(true);
+})
+
+test("Hitting a ship and checking the hit count", function() {
+    let ship = createShip(2);
+    ship.hit();
+    ship.hit();
+    expect(ship.getHits()).toBe(2);
 })
--- src/factories.js
+++ src/factories.js
@@ -1,5 +1,7 @@
 export function createShip(length, hits = 0) {
 
+    let id = length; 
+
     let getLength = () => {
         return length;
     };
@@ -18,10 +20,14 @@ export function createShip(length, hits = 0) {
         return hits >= length;
     }
 
-    return { getLength, setLength, getHits, hit, isSunk };
+    let getId = () => id;
+
+    return { getLength, setLength, getHits, hit, isSunk, getId };
 }
 
 export function createGameboard () {
+    let shotCoords = [];
+    let ships = [];
     let board = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -36,43 +42,65 @@ export function createGameboard () {
     ];
 
     let placeShip = (ship, row, col, orientation) => {
-            orientation = orientation.trim().toLowerCase();
-            if (orientation === "vertical") {
-                for (let i = row; i <= ship.getLength(); i++) {
-                    if (isPositionValid(i, col)) {
-                        board[i][col] = 1;
-                    } else {
-                        resetBoard(board);
-                        throw new Error("Position is not valid");
-                    }
+        if (ships.find(s => s === ship) !== undefined) {
+            throw new Error("Ship with this length already exists");
+        }
+        orientation = orientation.trim().toLowerCase();
+        if (orientation === "vertical") {
+            for (let k = row; k < row + ship.getLength(); k++) {
+                if (!isPositionValid(k, col)) {
+                    throw new Error("Position is not valid");
                 }
-            } else if (orientation === "horizontal") {
-                for (let i = col; i <= ship.getLength(); i++) {
-                    if (isPositionValid(row, i)) {
-                        board[row][i] = 1;
-                    } else {
-                        resetBoard(board);
-                        throw new Error("Position is not valid");
-                    }
-                }
-            } else {
-                throw new Error("Orientation is not valid");
             }
+            for (let i = row; i < row + ship.getLength(); i++) {
+                    board[i][col] = ship.getId();
+            }
+        } else if (orientation === "horizontal") {
+
+            for (let k = col; k < col + ship.getLength(); k++) {
+                if (!isPositionValid(row, k)) {
+                    throw new Error("Position is not valid");
+                }
+            }
+
+            for (let i = col; i < col + ship.getLength(); i++) {
+                    board[row][i] = ship.getId();
+            }
+        } else {
+            throw new Error("Orientation is not valid");
+        }
+        ships.push(ship);
         return board;
     }
 
-    //let receiveAttack
+    let receiveAttack = (x, y) => {
+        if (board[x][y] === -1) {
+            return false;
+        } 
+        if (board[x][y] === 0) {
+            shotCoords.push([x, y]);
+            board[x][y] = -1;
+            return false;
+        } else {
+            let ship = ships.find(ship => ship.getId() === board[x][y]);
+            ship.hit();
+            board[x][y] = -1;
+            return true;
+        } 
+    }
 
-    return {board, placeShip}
+    function isPositionValid (row, col) {
+        if (row < 0 || row > 9 || col < 0 || col > 9 || board[row][col] !== 0) {
+            return false;
+        }
+        return true;
+    
+    }
+
+    return {board, placeShip, receiveAttack}
     
 }
 
-function isPositionValid (row, col) {
-    if (row < 0 || row > 9 || col < 0 || col > 9) {
-        return false;
-    }
-    return true;
-}
 
 function resetBoard (board) {
     board = [
